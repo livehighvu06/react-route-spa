6b2eee0f46a0660f77cdbf913c0f631165deac07
Eddie Chen <livehighvu06@gmail.com>
2023-01-07 20:17:01 +0800

    部署測試

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Route from "./routes/Route";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
-
 const App = () => {
   return (
     <div>
-      <Link to="/home">Home</Link>
+      {process.env.REACT_APP_NOT_SECRET_CODE}
+      <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
 
